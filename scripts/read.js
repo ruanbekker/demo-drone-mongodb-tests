0f0a68069085bb7ca5757b86bbd120d8e51dabc4
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-05-01 23:15:06 +0200

    Update read.js

--- scripts/read.js
+++ scripts/read.js
@@ -1,2 +1,2 @@
-db.mycollection.find().count();
-db.mycollection.find({transaction_price: { \$gt: 970}}).forEach( printjson );
+db.mycollection.find().count()
+db.mycollection.find({transaction_price: { \$gt: 970}}).forEach( printjson )
